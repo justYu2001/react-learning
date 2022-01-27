87af7f705849c1f2dff5bc19ac772975662c1577
justYu2001 <jack4781012@gmail.com>
2022-01-27 17:56:34 +0800

    jsx basic

--- src/index.js
+++ src/index.js
@@ -3,11 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+const user1 = {
+  firstName: 'yu',
+  lastName: 'chen',
+  age: 20,
+};
+
+const user2 = {
+  firstName: 'jack',
+  lastName: 'chen',
+  age: 18,
+};
+
+const formatName = (user) => `${user.firstName} ${user.lastName}`;
+
+function isAdult(user) {
+  if(user.age >= 20) {
+    return <h2>You are an adult</h2>;
+  } else {
+    return <h2>You are a minor</h2>;
+  }
+}
+
+const element = (
+  <h1>
+    Hello, {formatName(user2)}
+  </h1>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <h1>Hello, world!</h1>,
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    {element}
+    {isAdult(user2)}
+    <div tabIndex='0' className="block"></div>
+  </div>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
